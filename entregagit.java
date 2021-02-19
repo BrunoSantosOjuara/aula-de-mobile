dac9bc93cca9df18a0ae4e7fe70dcd8483e23da1
Bruno dos Santos Araujo <bsa0206@gmail.com>
2021-02-19 14:59:01 -0200

    primeira versao

---
+++ entregagit.java
@@ -0,0 +1,21 @@
+public class EntregaGit{
+	public static void main(String[] args)
+	{
+		System.out.println("Entregando tarefas com o Git na linha de comando.");
+	}
+}
+
+import javax.swing.JOptionPane;
+public class EntregaGit{
+	public static void main(String[] args)
+	{
+		String nome = JOptionPane.ShowInputDialog("Qual o seu nome?");
+		JOptionPane.showMessageDialog(null, String.format("Bem Vindo, %s", nome));
+//outra funcionalidade
+//mais uma 
+//outra
+//mais uma agora
+
+			
+			}
+}
